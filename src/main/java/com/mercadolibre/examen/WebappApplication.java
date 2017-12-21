1ef85d75df1cb5f8071c80e680c3d0f80f7d7930
h3rnyx <hernan_ihf@hotmail.com>
2017-12-21 13:50:29 -0300

    Update WebappApplication.java

--- src/main/java/com/mercadolibre/examen/WebappApplication.java
+++ src/main/java/com/mercadolibre/examen/WebappApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class WebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
+	
+	@GetMapping("/")
+  	public String hello() {
+    		return "hello world!";
+  	}
 }
