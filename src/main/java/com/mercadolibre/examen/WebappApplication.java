67b80a1366e648cf9466653cea21c5c8e40dd415
h3rnyx <hernan_ihf@hotmail.com>
2017-12-21 13:54:46 -0300

    Update WebappApplication.java

--- src/main/java/com/mercadolibre/examen/WebappApplication.java
+++ src/main/java/com/mercadolibre/examen/WebappApplication.java
@@ -2,6 +2,7 @@ package com.mercadolibre.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
